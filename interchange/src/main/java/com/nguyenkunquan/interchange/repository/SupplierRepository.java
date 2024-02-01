371ea5f6d248a7b17d57db182dcd3b3287e30d4e
Nguyen Do Minh Quan <152289671+nguyenkunquan@users.noreply.github.com>
2024-02-02 03:26:09 +0700

    Delete interchange directory

--- interchange/src/main/java/com/nguyenkunquan/interchange/repository/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nguyenkunquan.interchange.repository;
-
-import com.nguyenkunquan.interchange.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-}
